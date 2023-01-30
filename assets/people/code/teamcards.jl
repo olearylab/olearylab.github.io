8a21d2e2d1cf5da8db76a1c88061d9eb502269fa
Antomek <41326016+Antomek@users.noreply.github.com>
2023-01-30 15:29:36 +0000

    Deploying to gh-pages from  @ 74aeca121638d5b07dc5a901830e8e5e0c6f7469 🚀

---
+++ assets/people/code/teamcards.jl
@@ -0,0 +1,20 @@
+# This file was generated, do not modify it. # hide
+#hideall
+past_team = [
+  (name="Saeed Aljaberi", pic="avatar.jpg", title="Ph.D. Student", vitae="", email=""),
+  (name="Adriano Bellotti", pic="avatar.jpg", title="Ph.D. Student", vitae="", email=""),
+  (name="Kristine Heiney", pic="Kristine.jpg", title="Ph.D. Student", vitae="", email="kh732@cam.ac.uk"),
+  (name="Adrianna R Loback", pic="avatar.jpg", title="Research Associate", vitae="", email=""),
+  (name="Dr. Dhruva Raman", pic="avatar.jpg", title="Research Associate", vitae="",  email="dvr23@cam.ac.uk")
+  ]
+
+"@@cards @@row" |> println
+
+for person in past_team
+  """
+  @@column
+    \\card{$(person.name)}{$(person.pic)}{$(person.title)}{$(person.vitae)}{$(person.email)}
+  @@
+  """ |> println
+end
+println("@@ @@") # end of cards + row
